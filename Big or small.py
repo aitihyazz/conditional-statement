5cd689648161ac45eed799b3c724358a3b8ebd6a
aitihyazz <aitihyasamadder@gmail.com>
2024-12-22 23:01:48 +0600

    Big or small

---
+++ Big or small.py
@@ -0,0 +1,9 @@
+i=int(input("Write a integer number:"))
+if(i<15):
+    print("The number is less then 15")
+    print("It is in if block")
+else:
+    print("The number is bigger than 15")
+    print("It is in else block")
+
+print("If i am not in if or else block")
